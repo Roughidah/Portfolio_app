5c29016be29dcf2d793a60a0f11dac95d8053ac5
Roughidah <sitirafidah10@gmail.com>
2025-07-13 22:24:10 +0800

    added gifs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,20 @@
 <script setup>
-// No imports needed for this simple component
+import { ref } from 'vue';
+
+const activeInterest = ref('');
+
+const interests = [
+  { name: 'Sewing', gif: 'https://media.tenor.com/ZDiB4QpY_OIAAAAM/sewing-sewing-machine.gif' },
+  { name: 'Swimming', gif: 'https://media.tenor.com/NoEDamB5vREAAAAM/david-hasselhoff.gif' },
+  { name: 'Reading manhwa', gif: 'https://media.tenor.com/sRpUJwQkMaIAAAAM/monkey-scrolling.gif' },
+  { name: 'Casual Gaming', gif: 'https://media.tenor.com/l-iCvGI-ynYAAAAM/playing-video-games-little-brother.gif' }
+];
 </script>
+
 
 <template>
   <div>
-    <h1>About Me 👋</h1>
+    <h1>About Raf 💁‍♀️</h1>
     <p>
       I'm a student learning web development, and I'm excited about building
       modern websites!
@@ -17,6 +27,19 @@
       <li>Vue.js for building dynamic applications</li>
       <li>Vue Router for navigation (like this website!)</li>
     </ul>
+
+    <h2>My interests (Click them!):</h2>
+    <ul>
+      <li v-for="interest in interests" :key="interest.name" @click="activeInterest = interest.gif"
+        style="cursor: pointer; margin-bottom: 10px;">
+        {{ interest.name }}
+      </li>
+    </ul>
+
+    <div v-if="activeInterest" class="highlight-box">
+      <img :src="activeInterest" alt="Interest GIF" style="max-width: 100%; border-radius: 5px;" />
+    </div>
+
 
     <div class="highlight-box">
       <p>
